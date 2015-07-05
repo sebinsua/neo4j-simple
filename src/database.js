bc6cc78d6da1f43558cd9c9b78c02e63862270e4
Seb Insua <me@sebinsua.com>
2015-07-05 19:12:39 +0100

    Auth.

--- src/database.js
+++ src/database.js
@@ -6,14 +6,31 @@ var Neo4j = require('rainbird-neo4j'),
     debug = require('debug')('neo4j-simple:database');
 
 var Promise = require('native-or-bluebird'),
-    thenifyAll = require('thenify-all'),
-    thenify = thenifyAll.thenify;
+    thenifyAll = require('thenify-all');
+
+var thenify = thenifyAll.thenify;
 
 var node = require('./node'),
     relationship = require('./relationship'),
     responseParser = require('./response-parser');
 
 var sourceifyPromises = require('./sourceify-promises');
+
+var decorateError = function decorateErrorWithName(error) {
+  var code = error.code;
+  switch (code) {
+    case 'Neo.ClientError.Security.AuthenticationFailed':
+    case 'Neo.ClientError.Security.AuthenticationRateLimit':
+    case 'Neo.ClientError.Security.AuthorizationFailed':
+      error.name = 'Neo4jAuthError';
+      break;
+    default:
+      error.name = 'Neo4jError';
+      break;
+  }
+
+  return error;
+};
 
 // This adds extra methods to the promises returned by Bluebird so that
 // we can use these in place of `then()`.
@@ -26,6 +43,18 @@ Neo4j.prototype = thenifyAll(Neo4j.prototype);
 module.exports = function (url, options) {
   options = options || {};
   options.idName = options.idName || 'id';
+  url = url || "http://localhost:7474/";
+
+  var hasCredentials = options.auth !== undefined;
+
+  var client;
+  if (hasCredentials) {
+    var username = options.auth.username;
+    var password = options.auth.password;
+    client = new Neo4j(url, username, password);
+  } else {
+    client = new Neo4j(url);
+  }
 
   var db = {};
 
@@ -35,14 +64,17 @@ module.exports = function (url, options) {
     NONE: null
   };
 
-  db.url = url || "http://localhost:7474/";
-  db.client = new Neo4j(db.url);
+  db.url = url;
+  db.client = client;
   db.idName = options.idName;
 
   db.Joi = Joi;
 
   db.begin = db.client.begin.bind(db.client);
-  db.query = db.client.query.bind(db.client);
+  db.query = function query(/* arguments */) {
+    var _client = db.client;
+    return _client.query.apply(db.client, arguments).error(decorateError);
+  };
   db.commit = db.client.commit.bind(db.client);
   db.rollback = db.client.rollback.bind(db.client);
   db.resetTimeout = db.client.resetTimeout.bind(db.client);
